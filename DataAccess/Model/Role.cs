193b7a7cb66779311e8037e073ad8ff9b6db1ef2
Edmaniak <adam.ouhrabka@gmail.com>
2019-02-27 17:06:28 +0100

    after init

---
+++ DataAccess/Model/Role.cs
@@ -0,0 +1,9 @@
+namespace DataAccess.Model
+{
+    public class Role
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Description { get; set; }
+    }
+}
--- DataAccess/Model/User.cs
+++ DataAccess/Model/User.cs
@@ -7,5 +7,6 @@ namespace DataAccess.Model
         public virtual int Id { get; set; }
         public virtual string Name { get; set; }
         public virtual string Surname { get; set; }
+        public virtual string Password { get; set; }
     }
 }
